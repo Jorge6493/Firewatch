8424989667d1bc82f4d31056c77e50886eef8616
Jorge6493 <jorge6493@gmail.com>
2020-09-22 00:15:30 -0400

    Usando nuevo dataset

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.metrics import categorical_crossentropy
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import confusion_matrix
+import numpy as np
 import itertools
 import os
 import shutil
@@ -15,6 +16,7 @@ import matplotlib.pyplot as plt
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+# workon keras_tflow
 
 train_path = 'fire/train'
 valid_path = 'fire/valid'
@@ -22,14 +24,14 @@ test_path = 'fire/test'
 
 batchSize = 10
 
-train_batches = ImageDataGenerator().flow_from_directory(directory=train_path, target_size=(224,224), color_mode="grayscale", classes=['fire', 'no-fire'], batch_size= batchSize)
-valid_batches = ImageDataGenerator().flow_from_directory(directory=valid_path, target_size=(224,224), color_mode="grayscale", classes=['fire', 'no-fire'], batch_size=batchSize)
+train_batches = ImageDataGenerator().flow_from_directory(directory=train_path, target_size=(224,224), color_mode="grayscale", classes=['fire', 'no-fire'], batch_size= batchSize, shuffle=True)
+valid_batches = ImageDataGenerator().flow_from_directory(directory=valid_path, target_size=(224,224), color_mode="grayscale", classes=['fire', 'no-fire'], batch_size=batchSize, shuffle=True)
 test_batches = ImageDataGenerator().flow_from_directory(directory=test_path, target_size=(224,224), color_mode="grayscale", classes=['fire', 'no-fire'], batch_size=batchSize, shuffle=False)
 
-assert train_batches.n == 780*2
-assert valid_batches.n == 220*2
-assert test_batches.n == 110*2
-assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 2
+# assert train_batches.n == 780*2
+# assert valid_batches.n == 220*2
+# assert test_batches.n == 110*2
+# assert train_batches.num_classes == valid_batches.num_classes == test_batches.num_classes == 2
 
 # print('Train Batches = '+train_batches.n)
 # print('Valid Batches = '+valid_batches.n)
@@ -37,6 +39,7 @@ assert train_batches.num_classes == valid_batches.num_classes == test_batches.nu
 
 stepsPerEpoch = train_batches.n/batchSize
 validationSteps = valid_batches.n/batchSize
+testSteps = test_batches.n/batchSize
 
 imgs, labels = next(train_batches)
 
@@ -48,10 +51,10 @@ def plotImages(images_arr):
 		ax.imshow(img)
 		ax.axis('off')
 	plt.tight_layout()
-	plt.show()
+	plt.show(block = True)
     
-# plotImages(imgs)
 # print(labels)
+# plotImages(imgs)
 
 model = Sequential([
 		Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same', input_shape=(224,224,1)),
@@ -68,4 +71,58 @@ model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentro
 
 model.fit(x=train_batches, validation_data=valid_batches, epochs=10, verbose=2, steps_per_epoch=stepsPerEpoch, validation_steps=validationSteps)
 
-# predict steps = stepsPerEpoch
+# test_imgs, test_labels = next(test_batches)
+# plotImages(test_imgs)
+# print(test_labels)
+
+print('testbatches.classes')
+print(test_batches.classes)
+# print(train_batches.classes)
+
+# print(valid_batches.classes)
+
+predictions = model.predict(x=test_batches,verbose=0,steps = testSteps)
+
+print('np')
+print(np.round(predictions))
+
+cm = confusion_matrix(y_true=test_batches.classes, y_pred=np.argmax(predictions, axis=-1))
+
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting normalize=True.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+test_batches.class_indices
+
+cm_plot_labels = ['fire', 'no-fire']
+plot_confusion_matrix(cm = cm, classes = cm_plot_labels, title = 'Confusion Matrix')
