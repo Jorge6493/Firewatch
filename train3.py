711daafe5e16a9f60fc5de844b518d00c5dcc9d4
Franz Pichardo Rodriguez <franz.pichardo.r@gmail.com>
2021-02-01 10:14:56 -0400

    Added prediction

--- train3.py
+++ train3.py
@@ -15,9 +15,11 @@ test_path = 'fire/test'
 training_datagen = ImageDataGenerator(rescale=1./255, zoom_range=0.15, horizontal_flip=True, fill_mode='nearest')
 validation_datagen = ImageDataGenerator(rescale = 1./255)
 
-train_generator = training_datagen.flow_from_directory(train_path, target_size=(224,224), shuffle = True, class_mode='categorical', batch_size = 128)
+train_generator = training_datagen.flow_from_directory(train_path, target_size=(224,224), classes=['fire', 'no-fire'], class_mode='categorical', shuffle = True, batch_size = 128)
 
-validation_generator = validation_datagen.flow_from_directory(valid_path, target_size=(224,224), class_mode='categorical', shuffle = True, batch_size= 14)
+validation_generator = validation_datagen.flow_from_directory(valid_path, target_size=(224,224), classes=['fire', 'no-fire'], class_mode='categorical', shuffle = True, batch_size= 14)
+
+test_generator = ImageDataGenerator().flow_from_directory(directory=test_path, target_size=(224,224), classes=['fire', 'no-fire'], class_mode='categorical', shuffle=False, batch_size=10)
 
 input_tensor = Input(shape=(224, 224, 3))
 base_model = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=False)
@@ -35,7 +37,7 @@ model = Model(inputs=base_model.input, outputs=predictions)
 for layer in base_model.layers:
   layer.trainable = False
   
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['acc'])
 
 history = model.fit(
 train_generator,
@@ -50,30 +52,75 @@ for layer in model.layers[:249]:
 for layer in model.layers[249:]:
   layer.trainable = True
 
-from tensorflow.keras.optimizers import SGD
-model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['acc'])history = model.fit(
+model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['acc'])history = model.fit(
 train_generator,
 steps_per_epoch = 14,
 epochs = 10,
 validation_data = validation_generator,
 validation_steps = 14)
 
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-	json_file.write(model_json)
+predictions = model.predict(x=test_batches,verbose=0,steps = testSteps)
 
-model.save_weights("model.h5")
-print("Saved model.")
+print('np')
+print(np.round(predictions))
+
+cm = confusion_matrix(y_true=test_generator.classes, y_pred=np.argmax(predictions, axis=-1))
+
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting normalize=True.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+
+test_batches.class_indices
+
+cm_plot_labels = ['fire', 'no-fire']
+plot_confusion_matrix(cm = cm, classes = cm_plot_labels, title = 'Confusion Matrix')
+
+#model_json = model.to_json()
+#with open("model.json", "w") as json_file:
+#	json_file.write(model_json)
+
+#model.save_weights("model.h5")
+#print("Saved model.")
 
 #load .json and create model
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-loaded_model = model_from_json(loaded_model_json)
+#json_file = open('model.json', 'r')
+#loaded_model_json = json_file.read()
+#json_file.close()
+#loaded_model = model_from_json(loaded_model_json)
 
-loaded_model.load_weights("model.h5")
-print("Loaded model.")
+#loaded_model.load_weights("model.h5")
+#print("Loaded model.")
 
-loaded_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001), metrics=['accuracy'])
+#loaded_model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.0001), metrics=['accuracy'])
 #score = loaded_model.evaluate(X, Y, verbose=0)
 #print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
