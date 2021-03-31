bf38bebeb8acc960201b81ebbc6e42c78100cef6
Franz Pichardo Rodriguez <franz.pichardo.r@gmail.com>
2021-03-31 01:17:55 -0400

    Removed Inceptionv3

--- binaryCross.py
+++ binaryCross.py
@@ -73,18 +73,14 @@ def plotImages(images_arr):
 # ])
 
 #incelption
-input_tensor = Input(shape=(224, 224, 3))
-base_model = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=False)
-
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(2048, activation='relu')(x)
-x = Dropout(0.25)(x)
-x = Dense(1024, activation='relu')(x)
-x = Dropout(0.2)(x)
-predictions = Dense(1, activation='sigmoid')(x)
-
-model = Model(inputs=base_model.input, outputs=predictions)
+model = Sequential([
+		Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same', input_shape=(224,224,1)),
+		MaxPool2D(pool_size=(2,2), strides=2),
+		Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding = 'same'),
+		MaxPool2D(pool_size=(2,2), strides=2),
+		Flatten(),
+		Dense(units=1, activation='sigmoid'),
+])
 
 for layer in base_model.layers:
   layer.trainable = False
